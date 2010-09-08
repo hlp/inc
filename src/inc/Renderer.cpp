96fb428d8c1f667167c1e3591e141a13ca8b9405
Patrick Tierney <patrick.l.tierney@gmail.com>
2010-09-08 12:47:20 -0400

    Added copyleft information

--- src/inc/Renderer.cpp
+++ src/inc/Renderer.cpp
@@ -1,4 +1,22 @@
 
+/*  Copyright (c) 2010, Patrick Tierney
+ *
+ *  This file is part of INC (INC's Not CAD).
+ *
+ *  INC is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  INC is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with INC.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <cinder/gl/gl.h>
 #include <cinder/params/Params.h>
 
