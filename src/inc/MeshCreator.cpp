993558374f887977e4ed2eed3b811fb7600314fc
Patrick Tierney <patrick.l.tierney@gmail.com>
2010-10-03 03:38:11 -0400

    Changed default scale for tripod meshes.

--- src/inc/MeshCreator.cpp
+++ src/inc/MeshCreator.cpp
@@ -38,7 +38,7 @@ MeshCreator::MeshCreator() {
 
     tripod_legs_ = 3;
     anemone_legs_ = 3;
-    tripod_mesh_scale_ = 10.0f;
+    tripod_mesh_scale_ = 15.0f;
     anemone_mesh_scale_ = 10.0f;
 
     current_mesh_ = std::tr1::shared_ptr<Solid>();
