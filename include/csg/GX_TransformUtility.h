c7163612539274b5f0a2f0a96244649d8b29a95a
Patrick Tierney <patrick.l.tierney@gmail.com>
2010-10-06 12:24:06 -0400

    Added CSG library, and modified library source so that it compiles with inc.

---
+++ include/csg/GX_TransformUtility.h
@@ -0,0 +1,18 @@
+
+// Author: Greg Santucci, 2008 and 2009
+// Email: thecodewitch@gmail.com
+// Web: http://createuniverses.blogspot.com/
+
+#ifndef GX_TRANSFORMUTILITY_H
+#define GX_TRANSFORMUTILITY_H
+
+#include "ML_Transform.h"
+
+namespace csg {
+
+void gxApplyTransformToOpenGL(mlTransform & trn);
+void gxMakeLookAtTransform(mlTransform * trn, const mlVector3D & vPos, const mlVector3D & vLookAt);
+
+}
+
+#endif // GX_TRANSFORMUTILITY_H
