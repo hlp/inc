2f77310f0f1b4cad45689c70d208a7219d71d9f2
Patrick Tierney <patrick.l.tierney@gmail.com>
2010-10-18 01:26:55 -0400

    Added pure virtual method to Exporter, write line.

--- include/inc/inc_DxfSaver.h
+++ include/inc/inc_DxfSaver.h
@@ -36,6 +36,8 @@ public:
 
     virtual void input_soft_solid(SoftSolid& solid) = 0;
 
+    virtual void write_line(const ci::Vec3f& v1, const ci::Vec3f& v2) = 0;
+
     virtual void set_layer(int) = 0;
     virtual void add_layer() = 0;
 };
@@ -57,11 +59,12 @@ public:
 
     //void write_file();
 
+    void write_line(const ci::Vec3f& v1, const ci::Vec3f& v2);
+
 private:
     void write_header();
     void write_footer();
     void write(std::string command, float value);
-    void write_line(const ci::Vec3f& v1, const ci::Vec3f& v2);
     void write_triangle(float v1x, float v1y, float v1z,
         float v2x, float v2y, float v2z,
         float v3x, float v3y, float v3z);
