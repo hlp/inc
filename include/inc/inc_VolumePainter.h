f4240755d1a148cb171e0924efc7796ec7082937
Patrick Tierney <patrick.l.tierney@gmail.com>
2010-11-24 12:21:44 -0500

    Added a basic test case for VolumePainter, still need to test it.

--- include/inc/inc_VolumePainter.h
+++ include/inc/inc_VolumePainter.h
@@ -1,7 +1,17 @@
 
 #pragma once
 
+#include <cinder/TriMesh.h>
+
 #include <inc/inc_Module.h>
+
+namespace toxi {
+namespace volume {
+class VolumetricSpaceVector;
+class RoundBrush;
+class HashIsoSurface;
+}
+}
 
 namespace inc {
 
@@ -13,6 +23,18 @@ public:
     void update();
     void draw();
 
+    float brush_radius_;
+    float iso_density_;
+    float iso_compute_;
+
+private:
+    void draw_volume();
+    void convert_volume_to_mesh();
+
+    std::shared_ptr<toxi::volume::VolumetricSpaceVector> volume_;
+    std::shared_ptr<toxi::volume::RoundBrush> brush_;
+    std::shared_ptr<toxi::volume::HashIsoSurface> iso_surface_;
+    std::shared_ptr<ci::TriMesh> volume_mesh_;
 };
 
 }
--- src/inc/inc_VolumePainter.cpp
+++ src/inc/inc_VolumePainter.cpp
@@ -1,13 +1,41 @@
 
+#include <cinder/gl/gl.h>
+
+#include <toxi/volume/toxi_volume_VolumetricSpaceVector.h>
+#include <toxi/volume/toxi_volume_RoundBrush.h>
+#include <toxi/volume/toxi_volume_HashIsoSurface.h>
+
 #include <inc/inc_VolumePainter.h>
 
 namespace inc {
 
 VolumePainter::VolumePainter() {
+    brush_radius_ = 5.0f;
+    iso_density_ = 1.0f;
+    iso_compute_ = 1.0f;
 }
 
 void VolumePainter::setup() {
     // nothing here
+    volume_ = std::shared_ptr<toxi::volume::VolumetricSpaceVector>(new 
+        toxi::volume::VolumetricSpaceVector(ci::Vec3f::one() * 10.0f,
+        100.0f, 100.0f, 100.0f));
+
+    brush_ = std::shared_ptr<toxi::volume::RoundBrush>(new
+        toxi::volume::RoundBrush(*(volume_.get()), brush_radius_));
+
+    // draw some stuff
+
+    convert_volume_to_mesh();
+}
+
+void VolumePainter::convert_volume_to_mesh() {
+    iso_surface_ = std::shared_ptr<toxi::volume::HashIsoSurface>(
+        new toxi::volume::HashIsoSurface(*(volume_.get()), iso_density_));
+
+    volume_mesh_ = std::shared_ptr<ci::TriMesh>(new ci::TriMesh());
+
+    iso_surface_->computeSurfaceMesh(volume_mesh_, iso_compute_);
 }
 
 void VolumePainter::update() {
@@ -15,7 +43,7 @@ void VolumePainter::update() {
 }
 
 void VolumePainter::draw() {
-    // nothing here
+    ci::gl::draw(*(volume_mesh_.get()));
 }
 
 }
