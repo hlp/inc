5fbc375faa76ace6952f98cb2787a6b6a48de185
Patrick Tierney <patrick.l.tierney@gmail.com>
2010-11-25 14:45:30 -0500

    Continuing to debug VolumePainter, fixed code in setSize in toxi::volume::RoundBrush, due to some typecasting error -- I would like to investigate this error: the cell sizes were being set to 0 even though there was a +1 in the code...

--- src/inc/inc_VolumePainter.cpp
+++ src/inc/inc_VolumePainter.cpp
@@ -11,16 +11,16 @@
 namespace inc {
 
 VolumePainter::VolumePainter() {
-    brush_radius_ = 5.0f;
+    brush_radius_ = 50.0f;
     iso_density_ = 1.0f;
-    iso_compute_ = 1.0f;
+    iso_compute_ = 3.0f * 0.001f;
 }
 
 void VolumePainter::setup() {
     // nothing here
     volume_ = std::shared_ptr<toxi::volume::VolumetricSpaceVector>(new 
-        toxi::volume::VolumetricSpaceVector(ci::Vec3f::one() * 10.0f,
-        10.0f, 10.0f, 10.0f));
+        toxi::volume::VolumetricSpaceVector(ci::Vec3f::one() * 400.0f,
+        18.0f, 18.0f, 18.0f));
 
     brush_ = std::shared_ptr<toxi::volume::RoundBrush>(new
         toxi::volume::RoundBrush(*(volume_.get()), brush_radius_));
@@ -33,18 +33,22 @@ void VolumePainter::setup() {
     brush_->drawAtAbsolutePos(ci::Vec3f(1.0f, 0.0f, 1.0f), d);
     brush_->drawAtAbsolutePos(ci::Vec3f(1.0f, 0.0f, 1.0f), d);
 
+    brush_->drawAtGridPos(1.0f, 1.0f, 1.0f, 1.0f);
+    brush_->drawAtGridPos(1.0f, 0.0f, 1.0f, 1.0f);
+
+
     convert_volume_to_mesh();
 }
 
 void VolumePainter::convert_volume_to_mesh() {
     iso_surface_ = std::shared_ptr<toxi::volume::HashIsoSurface>(
-        new toxi::volume::HashIsoSurface(*(volume_.get()), iso_density_));
+        new toxi::volume::HashIsoSurface(*(volume_.get())));
 
     volume_mesh_ = std::shared_ptr<ci::TriMesh>(new ci::TriMesh());
 
     iso_surface_->computeSurfaceMesh(volume_mesh_, iso_compute_);
 
-    ci::app::console() << "Mesh tris = " << volume_mesh_->getNumTriangles() << std::endl;
+    ci::app::console() << "Mesh triangles = " << volume_mesh_->getNumTriangles() << std::endl;
 }
 
 void VolumePainter::update() {
