9332ac68661cde688829939694e9f89c69d64ff9
Patrick Tierney <patrick.l.tierney@gmail.com>
2010-11-28 23:42:26 -0500

    Voronoi surface correctly partitions space, though post raising algorithms isn't as desired.

--- include/inc/inc_VoronoiSurface.h
+++ include/inc/inc_VoronoiSurface.h
@@ -5,6 +5,7 @@
 #include <utility> // for pair
 
 #include <cinder/Vector.h>
+#include <cinder/Rand.h>
 
 #include <inc/inc_Module.h>
 
@@ -24,10 +25,20 @@ public:
 private:
     void init();
     void load_random_points(int num_points);
+    void load_random_colors();
     void calculate_voronoi_ownership();
+    void calculate_max_dists();
     void create_lines_from_grid();
+    void raise_post_points();
+    void raise_post_points_lmap();
+
+    void bredth_first_raise_point(int index, ci::Vec2i coord);
+    void raise_point_recurse(ci::Vec2i coord);
     
     void loft_lines(Line line1, Line line2);
+
+    std::vector<float> last_height_;
+    std::vector<ci::Vec2i> coord_queue_;
 
     float max_post_height_;
     float post_unit_height_;
@@ -38,8 +49,12 @@ private:
     int loft_res_; // number of lines to loft with
 
     std::vector<ci::Vec2i> starting_coords_;
+    std::vector<ci::Color> coord_colors_;
+
+    std::vector<float> starting_coords_max_dists_;
     
     LineVector surface_lines_;
+    std::vector<ci::Color> surface_colors_;
     // the grid of the surface is essntially divided up into pixels, and 
     // each starting point has overship over these pixels.
     // int represents the index in the starting_coords_ vector
@@ -47,6 +62,8 @@ private:
 
     std::vector<std::vector<ci::Vec3f>> post_points_;
     std::vector<std::vector<bool>> grid_touched_;
+
+    ci::Rand rand;
 };
 
 }
--- src/inc/inc_VoronoiSurface.cpp
+++ src/inc/inc_VoronoiSurface.cpp
@@ -13,8 +13,8 @@ namespace inc {
 VoronoiSurface::VoronoiSurface() {
     max_post_height_ = 6.0f;
     post_unit_height_ = 1.0f;
-    grid_width_ = 40;
-    grid_depth_ = 100;
+    grid_width_ = 20;
+    grid_depth_ = 50;
     grid_unit_width_ = 4.0f;
     grid_unit_depth_ = 4.0f;
     loft_res_ = 6; 
@@ -23,13 +23,16 @@ VoronoiSurface::VoronoiSurface() {
 void VoronoiSurface::setup() {
     init();
 
-    load_random_points(10);
+    load_random_points(50);
+    load_random_colors();
 
-    // calculate voronoi from points
+    // calculate voronoi from points to know which point owns each post on the surface
     calculate_voronoi_ownership();
-    // now we know which point owns each post in the surface
+    
+    calculate_max_dists();
 
     // march along lines grid, outward from points, raising posts
+    raise_post_points();
 
     // stitch the post points into lines
     create_lines_from_grid();
@@ -65,20 +68,21 @@ void VoronoiSurface::load_random_points(int num_points) {
     boost::posix_time::ptime now(boost::posix_time::second_clock::universal_time());
     unsigned int seed = (now - epoch).total_seconds();
         
-    ci::Rand rand(seed);
+    rand.seed(seed);
 
     for (int i = 0; i < num_points; ++i) {
         starting_coords_.push_back(ci::Vec2i(rand.randInt(grid_width_),
             rand.randInt(grid_depth_)));
     }
-
-    /*
-    std::for_each(starting_coords_.begin(), starting_coords_.end(),
-        [&] (const ci::Vec2i& vec) {
-            post_points_[vec.x][vec.y].y = 5.0f;
-    } );
-    */
 }
+
+void VoronoiSurface::load_random_colors() {
+    for (int i = 0; i < starting_coords_.size(); ++i) {
+        coord_colors_.push_back(ci::Color(rand.randFloat(1.0f),
+            rand.randFloat(1.0f), rand.randFloat(1.0f)));
+    }
+}
+
 
 void VoronoiSurface::calculate_voronoi_ownership() {
     for (int xx = 0; xx < grid_width_; xx++) {
@@ -97,10 +101,36 @@ void VoronoiSurface::calculate_voronoi_ownership() {
     }
 }
 
+void VoronoiSurface::calculate_max_dists() {
+    starting_coords_max_dists_.resize(starting_coords_.size());
+
+    float max;
+    for (int i = 0; i < starting_coords_.size(); ++i) {
+        max = 0.0f;
+        for (int w = 0; w < grid_width_; ++w) {
+            for (int d = 0; d < grid_depth_; ++d) {
+                if (point_ownership_[w][d] != i)
+                    continue;
+
+                float dist = ci::Vec2f(w * grid_width_, d * grid_depth_).distance(
+                    ci::Vec2f(starting_coords_[i].x * grid_width_, 
+                    starting_coords_[i].y * grid_depth_));
+
+                if (dist > max)
+                    max = dist;
+            }
+        }
+
+        starting_coords_max_dists_[i] = max;
+    }
+}
+
 
 void VoronoiSurface::create_lines_from_grid() {
     for (int i = 1; i < grid_width_; ++i) {
         for (int j = 0; j < grid_depth_; ++j) {
+            surface_colors_.push_back(coord_colors_[point_ownership_[i][j]]);
+
             surface_lines_.push_back(Line(post_points_[i-1][j],
                 post_points_[i][j]));
         }
@@ -118,6 +148,11 @@ void VoronoiSurface::create_lines_from_grid() {
 
     for (int i = 1; i < grid_width_; ++i) {
         for (int j = 1; j < grid_depth_; ++j) {
+
+            for (int k = 0; k < loft_res_; ++k) {
+                surface_colors_.push_back(coord_colors_[point_ownership_[i][j]]);
+            }
+
             loft_lines(Line(post_points_[i-1][j-1], post_points_[i][j-1]),
                 Line(post_points_[i-1][j], post_points_[i][j]));
         }
@@ -133,7 +168,7 @@ void VoronoiSurface::loft_lines(Line line1, Line line2) {
 
     ci::Vec3f loft_start, loft_end;
 
-    for (int i = 0; i <= loft_res_; ++i) {
+    for (int i = 0; i < loft_res_; ++i) {
         loft_start = ci::Vec3f(
             ci::lmap<float>(i, 0, loft_res_, line_1_start.x, line_1_end.x),
             ci::lmap<float>(i, 0, loft_res_, line_1_start.y, line_1_end.y),
@@ -145,7 +180,38 @@ void VoronoiSurface::loft_lines(Line line1, Line line2) {
 
         surface_lines_.push_back(Line(loft_start, loft_end));
     }
+}
 
+void VoronoiSurface::raise_post_points() {
+    raise_post_points_lmap();
+}
+
+void VoronoiSurface::raise_post_points_lmap() {
+    for (int i = 0; i < starting_coords_.size(); ++i) {
+
+        for (int w = 0; w < grid_width_; ++w) {
+            for (int d = 0; d < grid_depth_; ++d) {
+                if (point_ownership_[w][d] != i)
+                    continue;
+
+                float dist = ci::Vec2f(w * grid_width_, d * grid_depth_).distance(
+                    ci::Vec2f(starting_coords_[i].x * grid_width_, 
+                    starting_coords_[i].y * grid_depth_));
+
+                post_points_[w][d].y = ci::math<float>::floor(ci::lmap<float>(
+                    dist, 0, starting_coords_max_dists_[i], 0, max_post_height_));
+            }
+        }
+
+    }
+}
+
+void VoronoiSurface::bredth_first_raise_point(int index, ci::Vec2i coord) {
+
+}
+
+void VoronoiSurface::raise_point_recurse(ci::Vec2i coord) {
+    // raise all neighboring points
 }
 
 void VoronoiSurface::update() {
@@ -159,11 +225,17 @@ void VoronoiSurface::draw() {
 
     glBegin(GL_LINES);
 
-    std::for_each(surface_lines_.begin(), surface_lines_.end(), [] 
-        (const std::pair<ci::Vec3f, ci::Vec3f>& line) {
-            glVertex3f(line.first.x, line.first.y, line.first.z);
-            glVertex3f(line.second.x, line.second.y, line.second.z);
-        } );
+    int i = 0;
+    for (LineVector::iterator it = surface_lines_.begin(); it != surface_lines_.end();
+        ++it) {
+
+        glColor3f(surface_colors_[i].r, surface_colors_[i].g, surface_colors_[i].b);
+
+        glVertex3f(it->first.x, it->first.y, it->first.z);
+        glVertex3f(it->second.x, it->second.y, it->second.z);
+
+        ++i;
+    }
 
     glEnd();
 }
